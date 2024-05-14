96d9b64c96606715921f6e2802b470feb30392ae
DAVID <davidopara2019@gmail.com>
2024-05-14 12:37:37 +0100

    updated routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -9,18 +9,23 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::group(['middleware' => 'auth:sanctum'], function(){
 
+Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
 Route::get('/index', [UserController::class, 'index'])->name('index');
 
 });
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'guest'], function (){
+
+Route::post('/register', [RegisteredUserController::class, 'store'])->name('register');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
+
 });
 
-Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest')->name('register');
-
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware('guest')->name('login');
 
 
