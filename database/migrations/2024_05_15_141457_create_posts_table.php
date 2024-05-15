21e2b11c618582b51361fa4e95400183925cd914
DAVID <davidopara2019@gmail.com>
2024-05-15 15:20:44 +0100

    Posts Updates

---
+++ database/migrations/2024_05_15_141457_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('categories_id');
+            $table->string('title');
+            $table->text('content');
+            $table->boolean('published')->default(false);
+            $table->foreignUuid('categories_id')->references('id')->on('Categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
